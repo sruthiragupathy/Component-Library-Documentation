dde658d0f0b583525ee3a65feb612f4c630d00f8
sruthiragupathy <sruthiragupathy@gmail.com>
2021-03-24 17:29:02 +0530

    refactor - Removed unwanted code from index.js

--- index.js
+++ index.js
@@ -4,20 +4,17 @@ const bodyEl = document.querySelector("body");
 
 //functions
 const hamburgerClickHandler = () => {
-    console.log("hi")
-    
-    hamburgerEl.classList.contains("click") ? 
-        hamburgerEl.classList.remove("click") : hamburgerEl.classList.add("click")
-    if(mobilemenuEl.classList.contains("menu__mobile")) { 
-        mobilemenuEl.classList.remove("menu__mobile")
-        bodyEl.classList.remove("hidden-overflow")
-     } 
-     else{
-        mobilemenuEl.classList.add("menu__mobile");
-        bodyEl.classList.add("hidden-overflow")
-     } 
-
-}
+  hamburgerEl.classList.contains("click")
+    ? hamburgerEl.classList.remove("click")
+    : hamburgerEl.classList.add("click");
+  if (mobilemenuEl.classList.contains("menu__mobile")) {
+    mobilemenuEl.classList.remove("menu__mobile");
+    bodyEl.classList.remove("hidden-overflow");
+  } else {
+    mobilemenuEl.classList.add("menu__mobile");
+    bodyEl.classList.add("hidden-overflow");
+  }
+};
 
 //EventListeners
-hamburgerEl.addEventListener("click",hamburgerClickHandler);
+hamburgerEl.addEventListener("click", hamburgerClickHandler);
