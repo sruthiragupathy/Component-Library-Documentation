173d5446262b1fd68796c4a65ffc70f211747dc5
sruthiragupathy <sruthiragupathy@gmail.com>
2021-05-06 13:09:49 +0530

    refactor - add some style refactors

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ const bodyEl = document.querySelector("body");
 
 //functions
 const hamburgerClickHandler = () => {
-  hamburgerEl.classList.contains("click")
-    ? hamburgerEl.classList.remove("click")
-    : hamburgerEl.classList.add("click");
-  if (mobilemenuEl.classList.contains("menu__mobile")) {
-    mobilemenuEl.classList.remove("menu__mobile");
-    bodyEl.classList.remove("hidden-overflow");
-  } else {
-    mobilemenuEl.classList.add("menu__mobile");
-    bodyEl.classList.add("hidden-overflow");
-  }
-};
+    console.log("hi")
+    
+    hamburgerEl.classList.contains("click") ? 
+        hamburgerEl.classList.remove("click") : hamburgerEl.classList.add("click")
+    if(mobilemenuEl.classList.contains("menu__mobile")) { 
+        mobilemenuEl.classList.remove("menu__mobile")
+        bodyEl.classList.remove("hidden-overflow")
+     } 
+     else{
+        mobilemenuEl.classList.add("menu__mobile");
+        bodyEl.classList.add("hidden-overflow")
+     } 
+
+}
 
 //EventListeners
-hamburgerEl.addEventListener("click", hamburgerClickHandler);
+hamburgerEl.addEventListener("click",hamburgerClickHandler);
